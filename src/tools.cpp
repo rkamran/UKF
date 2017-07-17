3a7ddef5255eaae74329f65e89c828b7153c864f
Rashid Kamran <rkamran@gmail.com>
2017-07-16 20:05:41 -0700

    Added UKF repo

---
+++ src/tools.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include "tools.h"
+
+using Eigen::VectorXd;
+using Eigen::MatrixXd;
+using std::vector;
+
+Tools::Tools() {}
+
+Tools::~Tools() {}
+
+VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
+                              const vector<VectorXd> &ground_truth) {
+	VectorXd rmse(4);
+	rmse << 0.0, 0.0, 0.0, 0.0;
+
+	if(estimations.size() == 0 or estimations.size() != ground_truth.size()){
+		cout << "Invalid Size" << endl;
+		return rmse;
+	}
+
+	for(int i=0; i < estimations.size(); ++i){
+		VectorXd residual = estimations[i] - ground_truth[i];
+		residual = residual.array() * residual.array();
+		rmse += residual;
+	}
+
+	rmse = rmse / estimations.size();
+	rmse = rmse.array().sqrt();
+	return rmse;
+}
